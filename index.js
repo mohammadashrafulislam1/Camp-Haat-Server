da3500e2748d137dc4801c73ca4c193dd4d2af78
mohammadashrafulislam <ixdell41@gmail.com>
2023-06-07 23:15:54 +0600

    add users

---
+++ index.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+require('dotenv').config()
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+const uri = `mongodb+srv://${process.env.USER_DB}:${process.env.USER_PASS}@cluster0.g2lboph.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Collections
+    const usersCollections = client.db('campDb').collection('users');
+
+    // users related api:
+    app.post('/users', async(req, res)=>{
+        const user = req.body;
+        const query = {email: user.email}
+        const existingUser =await usersCollections.findOne(query);
+        console.log('existing user', existingUser)
+        if(existingUser){
+            return res.send({message: 'User Already Exist'})
+        }
+        const result = await usersCollections.insertOne(user);
+        res.send(result)
+    })
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
+    res.send('Camp is Standing')
+  })
+  
+  app.listen(port, () => {
+    console.log(`Camp Haat is Standing on port ${port}`);
+  })
