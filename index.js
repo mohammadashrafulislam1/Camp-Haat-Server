bb4784b2f43ebaf71289a6d4fa0370e2ff5fb085
mohammadashrafulislam <ixdell41@gmail.com>
2023-06-09 16:25:04 +0600

    course related api: display

--- index.js
+++ index.js
@@ -87,6 +87,10 @@ async function run() {
      const result = await coursesCollections.insertOne(newItem)
      res.send(result)
     })
+    app.get('/courses', async(req, res)=>{
+      const result = await coursesCollections.find().toArray();
+      res.send(result)
+    })
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
