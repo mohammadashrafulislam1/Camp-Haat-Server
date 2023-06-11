aa076615f169d7ad8b0efe7c3f1d60d597a736f8
mohammadashrafulislam <ixdell41@gmail.com>
2023-06-11 19:37:05 +0600

    payment api

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ const port = process.env.PORT || 5000;
 // middleware
 app.use(cors());
 app.use(express.json());
-
 const verifyJWT = (req, res, next) =>{
   const authorization = req.headers.authorization;
   if(!authorization){
@@ -153,6 +152,20 @@ async function run() {
      const query = {_id: new ObjectId(id)}
       const result = await cartsCollections.deleteOne(query);
       res.send(result)
+    })
+    // Payment Related API
+    app.post('/create-payment-intent', async(req, res)=>{
+      const {price} = req.body;
+      const amount = price*100;
+      console.log(price, amount)
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: amount,
+        currency: 'usd',
+        payment_method_types: ['card']
+      })
+      res.send({
+        clientSecret: paymentIntent.client_secret
+      })
     })
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
